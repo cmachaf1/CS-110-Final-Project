2adcc87530cf98ca6152ea75a3779aa1c7fdbcaa
jackyallen52 <jackyallen52@gmail.com>
2017-05-08 15:36:10 -0400

    Update hiscoresSQL.py

--- GameBuildV1.5/hiscoresSQL.py
+++ GameBuildV1.5/hiscoresSQL.py
@@ -2,17 +2,17 @@ import sqlite3
 
 class HighScoreTable:
 	
-	def __init__(self)
+	def __init__(self):
 		Scores = sqlite3.connect("Landerscores.db")
 		cursor = connection.cursor()
 		cursor.execute("CREATE TABLE hiscore (score, int)")
 		cursor.execute("ALTER TABLE hiscore (name, str)")
 		
-	def addscore(self, name, points)
+	def addscore(self, name, points):
 		try:
 			cursor.execute("UPDATE hiscore SET score = points WHERE 'name' = true")
 		except Exception as err:
 			cursor.execute("INSERT INTO hiscore (name, score) VALUES (name, points)")
 		
-	def showscore(self)
+	def showscore(self):
 		cursor.execute("SELECT * FROM hiscore ORDER BY score DESC")
